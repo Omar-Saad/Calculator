84070cd91b0596f4f3509e4c89b9967109d65fd8
Omar Saad <es-omar.saad2019@alexu.edu.eg>
2020-11-05 11:15:18 +0200

    first commit

---
+++ app/src/main/java/com/omar/calculator/MainActivity.java
@@ -0,0 +1,22 @@
+package com.omar.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.arch.core.util.Function;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void buNumberEvent(View view) {
+
+
+
+}}
